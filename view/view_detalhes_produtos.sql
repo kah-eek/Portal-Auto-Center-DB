d681cb2d0fa5199307f9c8e43b8a747aecff58d0
CAIQUE MEIRA OLIVEIRA <16254840@CFP127ALUNO.LOCAL>
2018-05-22 09:49:14 -0300

    remove a obtecao dos feedback da view_detalhes_produtos

--- view/view_detalhes_produtos.sql
+++ view/view_detalhes_produtos.sql
@@ -22,10 +22,10 @@ cp.id_categoria_produto,
 cp.categoria,
 
 /* tbl_fabricante_produto */
-f.fabricante,
+f.fabricante
 
-/* tbl_feedback_cliente */
-fc.feedback
+/* tbl_feedback_cliente 
+fc.feedback*/
 
 FROM tbl_produto AS p
 
@@ -36,12 +36,12 @@ INNER JOIN tbl_modelo_produto AS mp ON mp.id_modelo_produto = p.id_modelo_produt
 INNER JOIN tbl_categoria_produto AS cp ON cp.id_categoria_produto = p.id_categoria_produto
 
 /* tbl_fabricante_produto */
-INNER JOIN tbl_fabricante_produto AS f ON f.id_fabricante_produto = mp.id_fabricante_produto
+INNER JOIN tbl_fabricante_produto AS f ON f.id_fabricante_produto = mp.id_fabricante_produto;
 
-/* tbl_pedido */
-INNER JOIN tbl_pedido AS pe ON pe.id_produto = pe.id_produto
+/* tbl_pedido 
+INNER JOIN tbl_pedido AS pe ON pe.id_produto = pe.id_produto;*/
 
-/* tbl_feedback_cliente */
-LEFT JOIN tbl_feedback_cliente AS fc ON fc.id_pedido = pe.id_pedido;
+/* tbl_feedback_cliente 
+LEFT JOIN tbl_feedback_cliente AS fc ON fc.id_pedido = pe.id_pedido;*/
 
 SELECT * FROM view_detalhes_produtos;
