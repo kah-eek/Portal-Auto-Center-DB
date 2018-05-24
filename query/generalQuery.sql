dbebc77ece52ca5b17e6226797de9e756b662c5a
CAIQUE MEIRA OLIVEIRA <16254840@CFP127ALUNO.LOCAL>
2018-05-24 11:25:18 -0300

    db backup

--- query/generalQuery.sql
+++ query/generalQuery.sql
@@ -2,5 +2,7 @@ USE dbautofast;
 
 SELECT * FROM tbl_cliente;
 SELECT * FROM tbl_endereco;
+SELECT * FROM tbl_modelo_produto;
 SELECT * FROM tbl_usuario;
 SELECT * FROM tbl_pedido;
+SELECT * FROM view_produto;
