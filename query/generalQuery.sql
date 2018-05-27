74d57d87f0b98a924a3e587140a95ba4fbde7967
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-05-27 12:23:30 -0300

    add view_veiculo_cliente

--- query/generalQuery.sql
+++ query/generalQuery.sql
@@ -1,11 +1,10 @@
 USE dbautofast;
 
 SELECT * FROM tbl_tipo_combustivel;
-DESCRIBE tbl_controle_abastecimento;
+SELECT * FROM tbl_controle_abastecimento;
 SELECT * FROM tbl_veiculo_cliente;
 SELECT * FROM tbl_cliente;
-SELECT * FROM tbl_veiculo;
 
-ALTER TABLE tbl_controle_abastecimento CHANGE COLUMN longitude longitude DOUBLE;
 
+select * from view_veiculo_cliente;
 
