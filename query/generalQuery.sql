20389dee401b47b9817d41bd3b560c48dd6855f8
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-05-27 21:14:44 -0300

    add valor padrao na latitude e longitude - tbl_controle_abastecimento

--- query/generalQuery.sql
+++ query/generalQuery.sql
@@ -1,3 +1,17 @@
 USE dbautofast;
 
+select * from tbl_tipo_combustivel;
+select * from tbl_controle_abastecimento;
+
+INSERT INTO tbl_controle_abastecimento 
+(
+	id_tipo_combustivel, 
+    id_veiculo_cliente,
+    valor_abastecido,
+    latitude,
+    longitude,
+)
+VALUES
+(
+);
 
