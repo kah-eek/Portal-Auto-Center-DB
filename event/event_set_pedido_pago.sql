e1c201305419260f4fa999e04a149b5400f666d8
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-02 00:18:42 -0300

    alter event to runn every 30 sends - event_insert_pedido_pago

--- event/event_set_pedido_pago.sql
+++ event/event_set_pedido_pago.sql
@@ -3,3 +3,5 @@ ON SCHEDULE
 EVERY 2 MINUTE
 DO
 UPDATE tbl_situacao_pedido SET id_tipo_situacao_pedido = 4 WHERE id_tipo_situacao_pedido = 3;
+
+ALTER EVENT event_set_pedido_pago ON SCHEDULE EVERY 30 SECOND;
