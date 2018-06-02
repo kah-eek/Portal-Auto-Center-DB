58008807ef6bd29bab919d59daa4a48d975e3cbe
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-02 00:19:16 -0300

    alter event to runn every 30 sends - event_set_pedido_pendente

--- event/event_set_pedido_pendente.sql
+++ event/event_set_pedido_pendente.sql
@@ -3,3 +3,5 @@ ON SCHEDULE
 EVERY 2 MINUTE
 DO
 UPDATE tbl_situacao_pedido SET id_tipo_situacao_pedido = 2 WHERE id_tipo_situacao_pedido = 4;
+
+ALTER EVENT event_set_pedido_pendente ON SCHEDULE EVERY 30 SECOND;
