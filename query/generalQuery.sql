da91fe9cc4c562a5fc284dd8f7e299a8a16f46b7
Caique M Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-08 21:30:43 -0300

    add insert states script

--- query/generalQuery.sql
+++ query/generalQuery.sql
@@ -1,19 +1,5 @@
-USE dbautofast;
+use caiqueoliveira;
 
-SELECT * FROM tbl_produto;
-desc tbl_endereco;
-SELECT * FROM tbl_situacao_pedido;
-SELECT * FROM tbl_tipo_situacao_pedido;
+select * from tbl_cliente;
 
 
-/*
-	usar este select para exibir os servicos confirmados e recusados pelo parceiro 
-    obs.: tratar os servicos recusados dos confirmados na aplicacao
-*/
-SELECT * FROM view_servico_prestado WHERE (id_tipo_situacao_pedido = 1 OR id_tipo_situacao_pedido = 6) AND id_cliente = 2;
-
-/*
-	usar este select para exibir os produtos enviados e envio recusados pelo parceiro 
-    obs.: tratar os produtos enviados dos nao enviados na aplicacao
-*/
-SELECT * FROM view_produtos_vendidos WHERE (id_tipo_situacao_pedido = 7 OR id_tipo_situacao_pedido = 9) AND id_cliente = 2;
